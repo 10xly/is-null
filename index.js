9773b43d78a22d68f82034667efc5d8ec3e487d1
tj-commits <thefoxjones@gmail.com>
2026-02-06 09:55:21 -0600

    safer

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+const falseValue = require("false-value")
+const isUndefined = require("@is-(unknown)/is-undefined")
+const RequireObjectCoercible = require("es-object-atoms/RequireObjectCoercible")
+const not = require("es-logical-not-operator")
+
 module.exports = function isNull(value) {
-  return value === null
+  if (isUndefined(value)) return falseValue()
+  try {
+    RequireObjectCoercible(value)
+    return falseValue()
+  } catch {
+    return not(value)
+  }
 }
